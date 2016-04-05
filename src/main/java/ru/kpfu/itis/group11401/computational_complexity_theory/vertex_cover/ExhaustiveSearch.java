740b88c5efed4b5bacd1cd4c6269cdeded30c8bf
Ed Akhmetshin <edmail32@gmail.com>
2016-04-05 22:52:52 +0400

    Refactor ExhaustiveSearch: move the code, that analyzes if set is a cover, to the metod 'boolean covers(BitSet, int[][])'

--- src/main/java/ru/kpfu/itis/group11401/computational_complexity_theory/vertex_cover/ExhaustiveSearch.java
+++ src/main/java/ru/kpfu/itis/group11401/computational_complexity_theory/vertex_cover/ExhaustiveSearch.java
@@ -7,24 +7,28 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 public class ExhaustiveSearch {
-    public static List<Set<Integer>> process(int[][] adjacencyMatrix) {
+    public static List<Set<Integer>> getCovers(int[][] adjacencyMatrix) {
         List<Set<Integer>> res = new ArrayList<>();
         int lastSetCode = (int) Math.pow(2, adjacencyMatrix.length);
         for (int setCode = 1; setCode < lastSetCode; setCode++) {
             BitSet set = convert(setCode);
-            boolean covers = true;
-            for (int i = 0; covers && i < adjacencyMatrix.length; i++) {
-                if (!set.get(i)) {
-                    for (int j = 0; j <= i; j++) {
-                        if (!set.get(j) && (adjacencyMatrix[i][j] > 0)) {
-                            covers = false;
-                            break;
-                        }
+            if (covers(set, adjacencyMatrix)) {
+                res.add(set.stream().boxed().collect(Collectors.toSet()));
+            }
+        }
+        return res;
+    }
+
+    private static boolean covers(BitSet set, int[][] adjacencyMatrix) {
+        boolean res = true;
+        for (int i = 0; res && i < adjacencyMatrix.length; i++) {
+            if (!set.get(i)) {
+                for (int j = 0; j <= i; j++) {
+                    if (!set.get(j) && adjacencyMatrix[i][j] > 0) {
+                        res = false;
+                        break;
                     }
                 }
-            }
-            if (covers) {
-                res.add(set.stream().boxed().collect(Collectors.toSet()));
             }
         }
         return res;
